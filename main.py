029b33af051293c0a58be2ef9b892937961e60d2
unknown <vladimirrr316@gmail.com>
2021-01-14 16:53:39 +0300

    telegram com

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 from aiogram import Bot, Dispatcher , executor
 
-bot = Bot('1265769369:AAHLQMOAuEztBsioqwmtJbjRpby_47JAi2A', parse_mode="HTML")
+bot = Bot('1095886841:AAEAusYeBylFFAeXISpC41R3MWObUbDOcbA', parse_mode="HTML")
 dp = Dispatcher(bot)
 
 if __name__ == '__main__':
-	from hend import dp
+	from npd import dp
+	from npd import scheduled
+	dp.loop.create_task(scheduled(300))
 	executor.start_polling(dp)
